befce0e71bf24bc03715ea20e6ef2b94d20dbf0c
pankajsharma81 <pankajsharma88279@gmail.com>
2025-11-24 16:48:16 +0530

    add user model

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  fullName: {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+  },
+  password: { type: String },
+}, { timestamps: true });
+
+const UserModel = mongoose.model("User", userSchema);
+
+module.exports = UserModel;
