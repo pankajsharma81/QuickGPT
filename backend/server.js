cd7f51dcee0aaa840125aca28fd392345583bedc
pankajsharma81 <pankajsharma88279@gmail.com>
2025-11-24 16:36:15 +0530

    add database

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 const app = require("./src/app");
+const connectToDB = require("./src/db/db");
+
+connectToDB();
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
