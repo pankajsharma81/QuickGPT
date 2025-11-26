f72b763fb655ee573a62f1ade993c6ffc3a3f2a9
pankajsharma81 <pankajsharma88279@gmail.com>
2025-11-26 10:21:38 +0530

    socket middleware

--- backend/src/sockets/server.socket.js
+++ backend/src/sockets/server.socket.js
@@ -1,8 +1,29 @@
 const { Server } = require("socket.io");
+const cookie = require("cookie");
+const jwt = require("jsonwebtoken");
+const UserModel = require("../models/user.model");
 
 function initSocketServer(httpServer) {
   const io = new Server(httpServer, {
     /* options */
+  });
+
+  io.use(async (socket, next) => {
+    const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
+
+    if (!cookies.token) {
+      return next(new Error("Authentication error : No token provided"));
+    }
+    try {
+      const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET);
+
+      const user = await UserModel.findById(decoded.id);
+
+      socket.user = user;
+      next();
+    } catch (err) {
+      next(new Error("Authentication error : Invalid token"));
+    }
   });
 
   io.on("connection", (socket) => {
@@ -10,4 +31,4 @@ function initSocketServer(httpServer) {
   });
 }
 
-module.exports = initSocketServer
+module.exports = initSocketServer;
